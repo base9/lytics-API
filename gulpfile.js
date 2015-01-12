790a76febeb3c2faeecc53f191a4d93837a3d5a3
Stephan Genyk <sgenyk91@gmail.com>
2015-01-11 19:35:14 -0800

    Adds gulp and streams jade, jshint, stylus, and watch

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp    = require('gulp');
+var gutil   = require('gulp-util');
+var nodemon = require('gulp-nodemon');
+var jshint  = require('gulp-jshint');
+var jade    = require('gulp-jade');
+var stylus  = require('gulp-stylus');
+
+var path = {
+
+};
+
+gulp.task('test', function() {
+  console.log('hello');
+});
+
+gulp.task('lint', function() {
+  return gulp.src([path._____])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('templates', function() {
+  gulp.src('./lib/*.jade')
+    .pipe(jade(path.________))
+    .pipe(gulp.dest(path._______))
+});
+
+gulp.task('one', function() {
+  gulp.src(path._____)
+    .pipe(stylus())
+    .pipe(gulp.dest(path._____));
+})
+
+gulp.watch('watch', function() {
+  gulpwatch(path._____, ['lint']);
+});
+
+gulp.task('default', ['lint', 'watch']);
